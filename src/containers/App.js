7411a072fd37787821e60ff1817cda5c8153395c
Kararin <charms_devil@mail.ru>
2016-05-15 17:47:34 +0300

    first commit

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router';
+import './styles.scss';
 
 export default class App extends Component {
-  render() {
-    return <div className='container'>Привет из App!</div>
-  }
+    render() {
+        return (
+            <div className='container'>
+                <ul className='nav nav-pills'>
+                    <li><Link to='/admin' activeClassName='active'>Админка</Link></li>
+                    <li><Link to='/list' activeClassName='active'>Список жанров</Link></li>
+                </ul>
+                {/* добавили вывод потомков */}
+                {this.props.children}
+            </div>
+        )
+    }
 }
